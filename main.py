5b64a072df6c8a3e07039432e3ec6d5abd60d246
Kayla200212 <99227847+Kayla200212@users.noreply.github.com>
2023-08-21 18:04:52 +0000

    added/fixed the tests

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ CMPS 2200  Preparation
 """
 
 def myfunction(input):
-	""" done. """
-	return input
+  print(myfunction('Hewwo world'))
+  """ done. """ 
+  return input
 
 
 
--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
 from main import *
 
 def test_myfunction():
-	""" done. """
-	assert myfunction('Hello') == 'Hello'
-	assert myfunction(1) == 1
-	assert myfunction(6) == -1
+  """ done. """
+  assert myfunction('Hello') == 'Hello'
+  assert myfunction(1) == 1
+  assert myfunction(6) == 6
